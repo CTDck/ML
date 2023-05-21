04dea85688c7e146a0c2ef2b1b6a005b99e106fb
CTDck <camerondick17@gmail.com>
2023-05-21 17:48:18 +0100

    Pylint Changes

--- data_generation.py
+++ data_generation.py
@@ -1,13 +1,22 @@
+"""Creates sinusoid class and methods for applying noise and fourier transforms to datasets"""
+
 import numpy as np
+
 
 # Create a clean data set of a specified size
 class Sinusoids:
-    def __init__(self, DATA_SIZE, ARRAY_SIZE) -> None:
-        self.data_size = DATA_SIZE
-        self.array_size = ARRAY_SIZE
-        pass
+    """Create sinusoids
+    """
+    def __init__(self, data_size, array_size) -> None:
+        self.data_size = data_size
+        self.array_size = array_size
 
     def create_data(self):
+        """Create dict of x, y and classifier values
+
+        Returns:
+            Dict: Contains corresponding x and y values along with their classification
+        """
         x_vals = [np.linspace(0, np.random.randint(1, 20)*np.pi, self.array_size)
               for i in range(self.data_size)]
         y_vals = [np.sin(x) for x in x_vals]
@@ -22,9 +31,26 @@ class Sinusoids:
         return entry
 
 # Create a noisy array from an array of input x-values
-def apply_noise(xarr, ARRAY_SIZE):
-    return [np.sin(x) + np.random.normal(0, .5, ARRAY_SIZE) for x in xarr]
+def apply_noise(xarr, array_size):
+    """Apply random noise to signla array
+
+    Args:
+        xarr (numpy array): input x values to aply noise transform to
+        array_size (int): size of array
+
+    Returns:
+        list: list of y values with noise applied
+    """
+    return [np.sin(x) + np.random.normal(0, .5, array_size) for x in xarr]
 
 # Apply the fourier transform to array
 def apply_ft(yarr):
+    """Apply Fourier Transform to signal array
+
+    Args:
+        yarr (numpy array): signal array
+
+    Returns:
+        numpy array: fourier transform of signal array
+    """
     return np.abs(np.fft.rfft(yarr))
